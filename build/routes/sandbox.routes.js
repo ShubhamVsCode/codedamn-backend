6c5d771485e6168755fbcfd0de98a8b3e9fdb79b
shubham <shubhamvscode@gmail.com>
2024-05-19 22:21:35 +0530

    feat: CI/CD added

--- build/routes/sandbox.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const sandbox_controller_1 = require("../controllers/sandbox.controller");
-const sandboxRouter = (0, express_1.Router)();
-sandboxRouter.post("/start", sandbox_controller_1.startSandbox);
-sandboxRouter.post("/stop", sandbox_controller_1.stopSandbox);
-exports.default = sandboxRouter;
